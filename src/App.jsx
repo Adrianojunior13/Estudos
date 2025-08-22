4f1d096c6092964153dbc07559cb120134a41249
Adrianojunior13 <a98054099@gmail.com>
2025-08-22 10:56:53 -0300

    Refatorar componente App para remover estado não utilizado e organizar importações

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import FunctionalComponent from './components/FunctionalComponent'
+import ClassComponent from './components/ClassComponent'
+import PropsExample from './components/PropsExample'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
+      <FunctionalComponent />
+      <ClassComponent />
+      <PropsExample name="Adriano" idade={30}  />
     </>
-  )
+  );
 }
 
 export default App
+
